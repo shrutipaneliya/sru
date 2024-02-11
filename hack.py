7a2a64d08efdfe1780a64bef9af3099fc5dabf20
shruti paneliya <shrutipaneliya06@gmail.com>
2024-02-11 20:16:09 +0530

    hack.py

---
+++ hack.py
@@ -0,0 +1,61 @@
+print("Enter the numbers: ")
+
+inp = tuple(map(int, input().split()))
+
+for i in inp:
+    print(i)
+
+print(type(inp))
+print(inp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+o,l,f=input().split()
+o=int(o)
+l=int(l)
+f=int(f)
+print(type(o))
+if o+l>=f:
+    print("yes")
+else:
+    print("no")   
+"""
+
+
